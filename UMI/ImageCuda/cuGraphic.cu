fe7f87d8fd5b141a446941a876687dac15f793a9
Uriel Malka <urielmalka1@gmail.com>
2025-08-21 21:42:08 +0300

    cuGraphic fix

--- UMI/ImageCuda/cuGraphic.cu
+++ UMI/ImageCuda/cuGraphic.cu
@@ -1,6 +1,5 @@
-#include <cuda_runtime.h>
-#include "../Image/Graphic.hpp"
-
+#include "cuGraphic.cuh"
+#include <iostream>
 
 __device__ Grayscale luminanceFormula(const RGB& color) {
     Grayscale g;
@@ -12,16 +11,70 @@ __device__ Grayscale luminanceFormula(const RGB& color) {
     return g;
 }
 
-__global__ void cuGray(RGB* rgb, Grayscale* gray)
-{
+__device__ Grayscale luminanceFormula(const BGR& color) {
+    Grayscale g;
+    g.I = static_cast<unsigned char>(
+        0.299f * color.r +
+        0.587f * color.g +
+        0.114f * color.b
+    );
+    return g;
+}
 
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    int j = blockIdx.y * blockDim.y + threadIdx.y;
-    int k = blockIdx.z * blockDim.z + threadIdx.z;
+__global__ void cuGrayK(Pixel* pixels_color, Pixel* pixels_gray, int w, int h) {
+    int x = blockIdx.x * blockDim.x + threadIdx.x; 
+    int y = blockIdx.y * blockDim.y + threadIdx.y; 
+    
+    if (x >= w || y >= h) return;
 
 
-    printf("[%d, %d, %d]\n",i,j, k);
+    int idx = y * w + x;
 
-    gray[i] = luminanceFormula(rgb[j]);
+    
+    Pixel input = pixels_color[idx];
+   
+    Grayscale result;
+
+    switch (input.type) {
+        case PixelType::RGB:
+            result = luminanceFormula(input.rgb);
+            break;
+        case PixelType::BGR:
+            result = luminanceFormula(input.bgr);
+            break;
+        case PixelType::Grayscale:
+            result = input.gray;
+            break;
+        default:
+            result = Grayscale{0};
+    }
+
+    pixels_gray[idx].type = PixelType::Grayscale;
+    pixels_gray[idx].gray = result;
 
 }
+
+
+void cuGray(Pixel* pixels, Pixel* h_grays, int w, int h, size_t bytes) {
+    
+    
+
+    Pixel *d_pixels, *d_grays;
+
+    cudaMalloc(&d_grays, bytes);
+    cudaMalloc(&d_pixels, bytes);
+
+    cudaMemcpy(d_pixels, pixels, bytes, cudaMemcpyHostToDevice);
+
+    dim3 threadsPerBlock(16, 16); // 256 threads per block
+    dim3 numBlocks((w + 15) / 16, (h + 15) / 16);
+   
+    cuGrayK<<<numBlocks, threadsPerBlock>>>(d_pixels, d_grays, w, h);
+    cudaDeviceSynchronize(); 
+ 
+    cudaMemcpy(h_grays, d_grays, bytes, cudaMemcpyDeviceToHost);
+
+    cudaFree(d_grays);
+    cudaFree(d_pixels);
+}
+
